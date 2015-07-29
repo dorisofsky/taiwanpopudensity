82a9b4410fa217c27ae69b901d253babbe801b2f
dorisofsky <dorisofsky@gmail.com>
2015-07-29 11:49:45 +0800

    試圖不要隱藏所有魚眼設計，用修改的?

--- index.js
+++ index.js
@@ -29,8 +29,9 @@ $(document).ready(function() {
     // var fisheye = d3.fisheye.circular().radius(100).distortion(2);
     var prj = function(v) {
       var ret = d3.geo.mercator().center([122,23.25]).scale(6000)(v);
- /*     var ret = fisheye({x:ret[0],y:ret[1]});
-      return [ret.x, ret.y];*/
+      var ret = {x:ret[0],y:ret[1]};
+      //var ret = fisheye({x:ret[0],y:ret[1]});
+      return [ret.x, ret.y];
     };
     var path = d3.geo.path().projection(prj);
     for(idx=features.length - 1;idx>=0;idx--) features[idx].density = density[features[idx].properties.C_Name];
@@ -45,10 +46,10 @@ $(document).ready(function() {
 
       });
     }
-/*    d3.select("svg").on("mousemove", function() {
-      fisheye.focus(d3.mouse(this));
+    d3.select("svg").on("mousemove", function() {
+      // fisheye.focus(d3.mouse(this));
       update();
-    });*/
+    });
     update();
   });
 });
